fec35bfdc857d0c7a12623f487b8759dc298a405
Edoka Isaac <edokaizkdaking247@gmail.com>
2025-06-12 00:13:59 +0100

    Updated Voting Contract With More Tests

--- src/voter.sol
+++ src/voter.sol
@@ -1,94 +1,173 @@
- // SPDX-License-Identifier: UNLICENSED
+    // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract VoterContract {
-    uint256 public numberOfCandidates;
+contract VotingContract {
     uint256 public totalVoters;
     uint256 public startTime;
     uint256 public stopTime;
-    address public admin;
+    address public INECChairman;
+    bool public isTesting;
 
-    error AdminOnly();
-    error InvalidVotingPeriod();
-    error VotingNotOpen();
-    error VoterUnderage();
-    error VoterAlreadyRegistered();
-    error VoterNotRegistered();
-    error NotVotersOwnCard();
-    error VoterHasVoted();
-    error VoterCardExpired();
-    error NotValidCandidate();
-    error CandidateAlreadyRegistered();
-    error InvalidCandidate();
+    error OnlyINEC(string reason);
+    error InvalidVotingPeriod(string reason);
+    error VotingNotOpen(string reason);
+    error VoterUnderage(string reason);
+    error VoterAlreadyRegistered(string reason);
+    error VoterNotRegistered(string reason);
+    error NotVotersOwnCard(string reason);
+    error VoterHasVoted(string reason);
+    error VoterCardExpired(string reason);
+    error NotValidCandidate(string reason);
+    error CandidateAlreadyRegistered(string reason);
+    error InvalidCandidate(string reason);
+    error ContractsNotAllowed(string reason);
+    error RescheduledStartTime(string reason);
+    error RescheduledStopTime(string reason);
+
+    enum Gender {
+        Male,
+        Female
+    }
+
+    enum Party {
+        APC,
+        PDP,
+        LP,
+        NNPP,
+        ADC
+    }
 
     struct Voter {
         address voterAddress;
         uint8 age;
-        uint256 cardExpiry;
+        uint256 voterCard;
         bool hasVoted;
-        bool isRegistered;
+        bool registered;
+        Gender gender;
+        uint256 registeredAt;
     }
 
     struct Candidate {
         address candidate;
         string fullName;
+        Party party;
         uint256 votes;
+        Gender gender;
     }
+
+    struct VotersGender {
+        uint256 males;
+        uint256 females;
+    }
+
+    address[] public votersList;
+    address[] public candidateList;
 
     mapping(address => Voter) public voters;
     mapping(address => Candidate) public candidates;
 
-    event Voted(address indexed voter, address indexed candidate, string candidateName);
+    event CandidateAdded(address indexed candidate, string fullName);
+    event VoterRegistered(address indexed voter);
+    event Voted(address indexed voter);
+    event VotingHasStarted();
+    event VotingHasEnded();
+    event ElectionRescheduled(uint256 startTime, uint256 stopTime);
 
     constructor(uint256 _startTime, uint256 _stopTime) {
-        if (stopTime > startTime) revert InvalidVotingPeriod();
         startTime = _startTime;
         stopTime = _stopTime;
-        admin = msg.sender;
+        INECChairman = msg.sender;
     }
 
-    modifier OnlyAdmin {
-        if (msg.sender != admin) revert AdminOnly();
+    function setTesting(bool _testing) external {
+        isTesting = _testing;
+    }
+
+    modifier INEC() {
+        if (msg.sender != INECChairman) revert OnlyINEC("Only INEC Can Call This Function");
         _;
     }
 
-
-    function registerVoter(address _voter, uint8 _age) external OnlyAdmin {
-        if (_age < 18) revert VoterUnderage();
-        if (voters[_voter].isRegistered) revert VoterAlreadyRegistered();
-
-        uint256 _cardExpiry = block.timestamp + 365 days;
-
-        voters[_voter] =  Voter({voterAddress:_voter, age:_age, cardExpiry: _cardExpiry, hasVoted: false, isRegistered: true});
-        
-
+    modifier OnlyEOAs() {
+        if (!isTesting && msg.sender != tx.origin) revert ContractsNotAllowed("Contracts Can Not Call This Function");
+        _;
     }
 
-    function addCandidates(address _candidateAddress, string memory _fullName) external OnlyAdmin {
+    function registerVoter(address _voter, uint8 _age, Gender _gender) external INEC {
+        if (_age < 18) revert VoterUnderage("Must Be Above 18 Years Of Age");
+        if (voters[_voter].registered) revert VoterAlreadyRegistered("Voter Has Already Been Registered");
+
+        uint256 _voterCard = block.timestamp + 365 days;
+
+        voters[_voter] = Voter({
+            voterAddress: _voter,
+            age: _age,
+            voterCard: _voterCard,
+            hasVoted: false,
+            registered: true,
+            gender: _gender,
+            registeredAt: block.timestamp
+        });
+
+        votersList.push(_voter);
+
+        totalVoters++;
+
+        emit VoterRegistered(_voter);
+    }
+
+    function addCandidates(address _candidateAddress, string memory _fullName, Party _party, Gender _gender)
+        external
+        INEC
+    {
         if (candidates[_candidateAddress].candidate != address(0)) {
-            revert CandidateAlreadyRegistered();
+            revert CandidateAlreadyRegistered("This Candidate Has Already Been Registered");
         }
 
-        candidates[_candidateAddress] = Candidate({candidate: _candidateAddress, fullName: _fullName, votes: 0});
+        candidates[_candidateAddress] =
+            Candidate({candidate: _candidateAddress, fullName: _fullName, party: _party, votes: 0, gender: _gender});
+
+        candidateList.push(_candidateAddress);
+
+        emit CandidateAdded(_candidateAddress, _fullName);
     }
 
-    function vote(address _candidate) external {
+    function vote(address _candidate) external OnlyEOAs {
+        if (stopTime <= startTime) revert InvalidVotingPeriod("The Stop Time Must Be Set After Start Time");
         if (block.timestamp < startTime || block.timestamp > stopTime) {
-            revert VotingNotOpen();
+            revert VotingNotOpen("Voting Is Not Currently Active");
         }
 
         Voter storage voter = voters[msg.sender];
-        if (!voter.isRegistered) revert VoterNotRegistered();
-        if (voter.voterAddress != msg.sender) revert NotVotersOwnCard();
-        if (voter.hasVoted) revert VoterHasVoted();
-        if (voter.cardExpiry <= block.timestamp) revert VoterCardExpired();
-        if (candidates[_candidate].candidate == address(0)) revert InvalidCandidate();
+        if (!voter.registered) revert VoterNotRegistered("Voter Has Not Been Registered");
+        if (voter.voterAddress != msg.sender) revert NotVotersOwnCard("Voter Using A Different Voter's Card");
+        if (voter.hasVoted) revert VoterHasVoted("Voter Has Already Voted");
+        if (voter.voterCard <= block.timestamp) revert VoterCardExpired("Voter's Card Has Expired");
+        if (!validCandidate(_candidate)) revert InvalidCandidate("Candidate Is Not Registered");
+        // if (candidates[_candidate].candidate == address(0)) revert InvalidCandidate("Candidate Is Not Registered");
 
         voter.hasVoted = true;
         candidates[_candidate].votes++;
 
-        emit Voted(msg.sender, _candidate, candidates[_candidate].fullName);
-        
+        emit Voted(msg.sender);
+    }
+
+    function rescheduleElection(uint256 _startTime, uint256 _stopTime) external INEC {
+        if (_startTime <= block.timestamp) {
+            revert RescheduledStartTime("Rescheduled Start Time Must Be Later In The Future");
+        }
+        if (_stopTime <= _startTime) revert RescheduledStopTime("Rescheduled Stop Time Must Be After Start Time");
+        startTime = _startTime;
+        stopTime = _stopTime;
+
+        emit ElectionRescheduled(_startTime, _stopTime);
+    }
+
+    function validCandidate(address _candidate) public view returns (bool) {
+        for (uint256 i = 0; i < candidateList.length; i++) {
+            if (candidateList[i] == _candidate) return true;
+        }
+        return false;
     }
 
     function getCandidate(address _candidate) external view returns (string memory fullName, uint256 votes) {
@@ -101,10 +180,57 @@ contract VoterContract {
     }
 
     function getVoterCardExpiry(address _voter) external view returns (uint256) {
-        return voters[_voter].cardExpiry;
+        return voters[_voter].voterCard;
     }
 
-    function getTotalVoters() external view returns (uint256) {
+    function getTotalRegisteredVoters() external view returns (uint256) {
         return totalVoters;
+    }
+
+    function getGenderCountOfVoters() external view returns (VotersGender memory result) {
+        for (uint256 i = 0; i < votersList.length; i++) {
+            Gender gender = voters[votersList[i]].gender;
+
+            if (gender == Gender.Male) result.males++;
+            else if (gender == Gender.Female) result.females++;
+        }
+    }
+
+    function getResults() external view returns (Candidate[] memory) {
+        Candidate[] memory results = new Candidate[](candidateList.length);
+
+        for (uint256 i = 0; i < candidateList.length; i++) {
+            address candidate = candidateList[i];
+            results[i] = candidates[candidate];
+        }
+        return results;
+    }
+
+    // If two candidates both have the highests number of votes, both candidates will be returned
+    function getWinners() external view returns (Candidate[] memory) {
+        Candidate[] memory temp = new Candidate[](candidateList.length);
+        uint256 highestVotes;
+        uint256 winnerCount;
+
+        // Loop to get the highest vote count
+        for (uint256 i = 0; i < candidateList.length; i++) {
+            uint256 votes = candidates[candidateList[i]].votes;
+
+            if (votes > highestVotes) {
+                highestVotes = votes;
+                winnerCount = 1;
+                temp[0] = candidates[candidateList[i]];
+            } else if (votes == highestVotes) {
+                temp[winnerCount] = candidates[candidateList[i]];
+                winnerCount++;
+            }
+        }
+
+        // Get Winner(s)
+        Candidate[] memory winners = new Candidate[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            winners[i] = temp[i];
+        }
+        return winners;
     }
 }
--- test/voter.t.sol
+++ test/voter.t.sol
@@ -2,124 +2,173 @@
 pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
-import {VoterContract} from "../src/voter.sol";
+import {VotingContract} from "../src/voter.sol";
 
 contract VoterContractTest is Test {
-    VoterContract public voterContract;
-
-    address admin = address(0xABCD);
-    address voter1 = address(0x1001);
-    address voter2 = address(0x1002);
-    address candidate1 = address(0x2001);
-    address candidate2 = address(0x2002);
-
-    uint256 start;
-    uint256 stop;
+    VotingContract public voting;
+    address public INECChairman;
+    address public voter1;
+    address public voter2;
+    address public candidate1;
+    address public candidate2;
+    uint256 deployedAt;
 
     function setUp() public {
-        start = block.timestamp + 1 days;
-        stop = block.timestamp + 3 days;
-        vm.prank(admin);
-        voterContract = new VoterContract(start, stop);
+        vm.warp(400 days);
+        INECChairman = address(this);
+        voter1 = address(0x1);
+        voter2 = address(0x2);
+        candidate1 = address(0x3);
+        candidate2 = address(0x4);
+        deployedAt = block.timestamp;
+
+        voting = new VotingContract(deployedAt, deployedAt + 1 days);
+        voting.setTesting(true);
+    }
+
+    function testOnlyChairmanCanRegisterVoters() public {
+        vm.prank(voter1);
+        vm.expectRevert(abi.encodeWithSelector(VotingContract.OnlyINEC.selector, "Only INEC Can Call This Function"));
+
+        voting.registerVoter(voter2, 18, VotingContract.Gender.Male);
+
+        console.log("startTime:", voting.startTime());
+        console.log("stopTime:", voting.stopTime());
+    }
+
+    function testRegisterUnderageVoter() public {
+        vm.expectRevert(abi.encodeWithSelector(VotingContract.VoterUnderage.selector, "Must Be Above 18 Years Of Age"));
+        voting.registerVoter(voter2, 12, VotingContract.Gender.Male);
     }
 
     function testRegisterVoter() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 25);
-
-        (string memory name, uint256 votes) = voterContract.getCandidate(candidate1);
-        assertEq(votes, 0); // candidate not yet added
+        console.log(msg.sender);
+        vm.prank(voter1);
+        voting.registerVoter(voter1, 19, VotingContract.Gender.Male);
+        (,, uint256 voterCard,, bool registered,,) = voting.voters(voter1);
+        assertTrue(registered);
+        assertGt(voterCard, block.timestamp);
     }
 
-    function testCannotRegisterUnderage() public {
-        vm.prank(admin);
-        vm.expectRevert(VoterContract.VoterUnderage.selector);
-        voterContract.registerVoter(voter1, 16);
+    function testRegisterSameVoterTwice() public {
+        voting.registerVoter(voter1, 19, VotingContract.Gender.Male);
+        vm.expectRevert(
+            abi.encodeWithSelector(VotingContract.VoterAlreadyRegistered.selector, "Voter Has Already Been Registered")
+        );
+        voting.registerVoter(voter1, 19, VotingContract.Gender.Male);
     }
 
     function testAddCandidate() public {
-        vm.prank(admin);
-        voterContract.addCandidates(candidate1, "Alice");
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
 
-        (string memory name, uint256 votes) = voterContract.getCandidate(candidate1);
-        assertEq(name, "Alice");
+        (, string memory fullName, VotingContract.Party party, uint256 votes, VotingContract.Gender gender) =
+            voting.candidates(candidate1);
+
+        assertEq(fullName, "Goodluck Ebele Jonathan");
         assertEq(votes, 0);
+        assertEq(uint8(party), uint8(VotingContract.Party.PDP));
+        assertEq(uint8(gender), uint8(VotingContract.Gender.Male));
     }
 
-    function testCannotVoteBeforeVotingStart() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 30);
-        vm.prank(admin);
-        voterContract.addCandidates(candidate1, "Alice");
-
-        vm.prank(voter1);
-        vm.expectRevert(VoterContract.VotingNotOpen.selector);
-        voterContract.vote(candidate1);
+    function testAddDuplicateCandidate() public {
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                VotingContract.CandidateAlreadyRegistered.selector, "This Candidate Has Already Been Registered"
+            )
+        );
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
     }
 
-    function testSuccessfulVote() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 22);
-        vm.prank(admin);
-        voterContract.addCandidates(candidate1, "Alice");
+    function testCandidateIsValid() public {
+        address invalidCandidate = address(0x111);
 
-        vm.warp(start + 1);
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
+        assertTrue(voting.validCandidate(candidate1));
+        assertFalse(voting.validCandidate(invalidCandidate));
+    }
+
+    function testVoting() public {
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
+        voting.registerVoter(voter1, 30, VotingContract.Gender.Male);
 
         vm.prank(voter1);
-        voterContract.vote(candidate1);
+        voting.vote(candidate1);
 
-        (, uint256 votes) = voterContract.getCandidate(candidate1);
+        (,,, bool hasVoted,,,) = voting.voters(voter1);
+        (,,, uint256 votes,) = voting.candidates(candidate1);
+
+        assertTrue(hasVoted);
         assertEq(votes, 1);
     }
 
-    function testDoubleVotingFails() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 22);
-        vm.prank(admin);
-        voterContract.addCandidates(candidate1, "Alice");
+    function testUnregisteredVoterVoting() public {
+        voting.addCandidates(candidate2, "Patience Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Female);
 
-        vm.warp(start + 1);
-        vm.prank(voter1);
-        voterContract.vote(candidate1);
-
-        vm.prank(voter1);
-        vm.expectRevert(VoterContract.VoterHasVoted.selector);
-        voterContract.vote(candidate1);
+        vm.prank(voter2);
+        vm.expectRevert(
+            abi.encodeWithSelector(VotingContract.VoterNotRegistered.selector, "Voter Has Not Been Registered")
+        );
+        voting.vote(candidate2);
     }
 
-    function testVotingWithExpiredCardFails() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 22);
-        vm.prank(admin);
-        voterContract.addCandidates(candidate1, "Alice");
+    function testVoterCardExpired() public {
+        vm.warp(deployedAt - 366 days);
+        voting.registerVoter(voter2, 29, VotingContract.Gender.Female);
 
-        uint256 expiry = voterContract.getVoterCardExpiry(voter1);
-        vm.warp(expiry + 1);
+        vm.warp(deployedAt + 1 hours);
+
+        voting.addCandidates(candidate2, "Patience Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Female);
+
+        vm.prank(voter2);
+        vm.expectRevert(abi.encodeWithSelector(VotingContract.VoterCardExpired.selector, "Voter's Card Has Expired"));
+        voting.vote(candidate2);
+    }
+
+    function testVoterVotingTwice() public {
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
+        voting.registerVoter(voter1, 30, VotingContract.Gender.Male);
 
         vm.prank(voter1);
-        vm.expectRevert(VoterContract.VoterCardExpired.selector);
-        voterContract.vote(candidate1);
-    }
+        voting.vote(candidate1);
 
-    function testInvalidCandidateFails() public {
-        vm.prank(admin);
-        voterContract.registerVoter(voter1, 22);
-
-        vm.warp(start + 1);
         vm.prank(voter1);
-        vm.expectRevert(VoterContract.InvalidCandidate.selector);
-        voterContract.vote(candidate2);
+        vm.expectRevert(abi.encodeWithSelector(VotingContract.VoterHasVoted.selector, "Voter Has Already Voted"));
+        voting.vote(candidate1);
     }
 
-    function testOnlyAdminCanAddCandidates() public {
-        vm.expectRevert(VoterContract.AdminOnly.selector);
-        voterContract.addCandidates(candidate1, "Alice");
-    }
+    function testGetResultsAndWinners() public {
+        voting.addCandidates(
+            candidate1, "Goodluck Ebele Jonathan", VotingContract.Party.PDP, VotingContract.Gender.Male
+        );
+        voting.addCandidates(candidate2, "Muhammadu Buhari", VotingContract.Party.APC, VotingContract.Gender.Male);
 
-    function testOnlyAdminCanRegisterVoter() public {
-        vm.expectRevert(VoterContract.AdminOnly.selector);
-        voterContract.registerVoter(voter1, 22);
+        voting.registerVoter(voter1, 19, VotingContract.Gender.Male);
+        voting.registerVoter(voter2, 18, VotingContract.Gender.Male);
+
+        vm.prank(voter1);
+        voting.vote(candidate1);
+
+        vm.prank(voter2);
+        voting.vote(candidate2);
+
+        VotingContract.Candidate[] memory results = voting.getResults();
+        // assertEq(results.length, 2);
+        assertEq(results[0].votes + results[1].votes, 2);
+
+        VotingContract.Candidate[] memory winners = voting.getWinners();
+        assertEq(winners.length, 2);
     }
 }
-
-
